076c2e4b854c66b42c55df69efe3f6354a544c6d
Chris Mann <chrispmann@Thinkpad-Senbei.localdomain>
2022-08-12 18:09:19 -0700

    commit tree f2c0f6f717fc8b67ae2283e4ec31f88a038d2b40

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+    }
+    return messages; 
+}
+
+function countDown(n){
+    for (let i = n; i >= 0; i--){
+            console.log(i)
+            debugger;
+    }
+}
+
+countDown(10);
